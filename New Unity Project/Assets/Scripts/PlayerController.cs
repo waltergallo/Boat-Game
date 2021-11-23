c3bbe1f9f20e57786f6da68f29c433daf6cd9eff
waltergallo <walter.celio.gallo.n@icloud.com>
2021-11-23 09:30:14 -0300

    Enabled Boost and Boost Hop

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f;
-    public float turnSpeed = 30.0f;
-    public float horizontalInput;
-    public float forwardInput;
+    private Rigidbody playerRB;
+    private float speed = 30.0f;
+    private float turnSpeed = 30.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public bool isOnGround = true;
+    public float jumpforce = 10;
+    public float gravityModifyer;
+    public bool hasBoost = false;
 
-    // Start is called before the first frame update
-    void Start()
+        // Start is called before the first frame update
+        void Start()
     {
+
+        Physics.gravity *= gravityModifyer;
+        playerRB = GetComponent<Rigidbody>();
 
     }
 
@@ -21,10 +29,60 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        Debug.Log(horizontalInput);
 
         transform.Translate(Vector3.right * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && hasBoost)
+        {
+
+            playerRB.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isOnGround = false;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+
+            isOnGround = true;
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Boost"))
+        {
+
+            Boost();
+            Destroy(other.gameObject);
+            StartCoroutine(BoostCountdownRoutine());
+            hasBoost = true;
+
+        }
+
+    }
+
+    IEnumerator BoostCountdownRoutine()
+    {
+
+        yield return new WaitForSeconds(10);
+        speed = 30;
+        hasBoost = false;
+
+    }
+
+    private void Boost()
+    {
+
+        speed = 50;
 
     }
 }
